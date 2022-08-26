4af57742065abbcb6d57afa1dc75e1fe7399854f
khamza <khamza.khudayberdiyev@gmail.com>
2022-08-26 08:08:41 +0500

    first commit

---
+++ app/Models/Tag.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+
+    public $timestamps = false;
+
+    protected $table   = 'tags';
+    protected $guarded = [];
+
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class);
+    }
+}
